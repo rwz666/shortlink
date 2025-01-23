8ca71437a3b0480e00dcbe529f59eb9075813127
任五州 <3116419886@qq.com>
2025-01-23 18:36:45 +0800

    feature：开发短链接后管系统通用持久层实体对象属性填充

---
+++ admin/src/main/java/com/offer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,32 @@
+package com.offer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @author rwz
+ * @since 2025/1/23
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("开始插入填充...");
+        strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+        strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("开始更新填充...");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
