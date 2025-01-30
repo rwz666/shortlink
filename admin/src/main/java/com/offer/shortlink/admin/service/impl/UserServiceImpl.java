5d3c85d8c945dcdd3b4064aed90ef12f8b85b19f
任五州 <3116419886@qq.com>
2025-01-31 00:06:21 +0800

    feature：优化用户新增提示报错

--- admin/src/main/java/com/offer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/offer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -22,6 +22,7 @@ import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -67,11 +68,15 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
         try {
             if (lock.tryLock()) {
-                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-                if (inserted < 1) {
+                try {
+                    int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                    if (inserted < 1) {
+                        throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                    }
+                    userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                }catch (DuplicateKeyException e) {
                     throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
                 }
-                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
                 return;
             }
             throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
