ca2b127bd768b7b3f32948b35565daa63a14398d
任五州 <3116419886@qq.com>
2025-02-04 19:07:10 +0800

    feature：短连接服务修改短链接实体优化import

--- project/src/main/java/com/offer/shortlink/project/dto/req/ShortLinkUpdateReqDTO.java
+++ project/src/main/java/com/offer/shortlink/project/dto/req/ShortLinkUpdateReqDTO.java
@@ -1,10 +1,7 @@
 package com.offer.shortlink.project.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
