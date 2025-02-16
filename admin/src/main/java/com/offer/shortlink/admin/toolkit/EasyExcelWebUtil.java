3269bb7ca895046049dbe45b2be86149fb46740a
任五州 <3116419886@qq.com>
2025-02-16 21:58:49 +0800

    feature：开发短链接批量创建功能EasyExcel工具类write方法注释

--- admin/src/main/java/com/offer/shortlink/admin/toolkit/EasyExcelWebUtil.java
+++ admin/src/main/java/com/offer/shortlink/admin/toolkit/EasyExcelWebUtil.java
@@ -15,13 +15,22 @@ import java.util.List;
  */
 public class EasyExcelWebUtil {
 
+
+    /**
+     * 向浏览器写入Excel响应，直接返回用户下载数据
+     *
+     * @param response 响应
+     * @param fileName 文件名
+     * @param clazz    指定写入类
+     * @param data     写入数据
+     */
     @SneakyThrows
     public static void write(HttpServletResponse response, String fileName, Class<?> clazz, List<?> data) {
         // 设置响应头
         response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         response.setCharacterEncoding("UTF-8");
         fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
-        response.setHeader("Content-Disposition", "attachment; filename*="+ fileName +".xlsx");
+        response.setHeader("Content-Disposition", "attachment; filename*=" + fileName + ".xlsx");
 
         EasyExcel.write(response.getOutputStream(), clazz).sheet("Sheet").doWrite(data);
     }
