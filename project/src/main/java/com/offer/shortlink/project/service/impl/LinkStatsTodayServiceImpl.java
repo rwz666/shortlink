2c2817c75e30c7f7f714d08c75994b7eca456233
任五州 <3116419886@qq.com>
2025-02-18 18:09:38 +0800

    fix：短链接记录变更分组功能_今日监控实现类添加Service注解

--- project/src/main/java/com/offer/shortlink/project/service/impl/LinkStatsTodayServiceImpl.java
+++ project/src/main/java/com/offer/shortlink/project/service/impl/LinkStatsTodayServiceImpl.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.offer.shortlink.project.dao.entity.LinkStatsTodayDO;
 import com.offer.shortlink.project.dao.mapper.LinkStatsTodayMapper;
 import com.offer.shortlink.project.service.LinkStatsTodayService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author rwz
  * @since 2025/2/17
  * 短链接今日访问接口层实现类
  */
+@Service
 public class LinkStatsTodayServiceImpl extends ServiceImpl<LinkStatsTodayMapper, LinkStatsTodayDO> implements LinkStatsTodayService {
 }
