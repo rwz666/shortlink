af58887e586fc689846d5390e71ceea5f6f7fce0
任五州 <3116419886@qq.com>
2025-02-23 22:26:40 +0800

    fix: 修复查询单个短链接监控数据中一周数据统计从0开始的错误

--- project/src/main/java/com/offer/shortlink/project/service/impl/ShortLinkStatsServiceImpl.java
+++ project/src/main/java/com/offer/shortlink/project/service/impl/ShortLinkStatsServiceImpl.java
@@ -78,7 +78,7 @@ public class ShortLinkStatsServiceImpl implements ShortLinkStatsService {
         //一周分布访问详情
         List<LinkAccessStatsDO> listWeekdayStatsByShortLink = linkAccessStatsMapper.listWeekdayStatsByShortLink(requestParam);
         ArrayList<Integer> weekdayStats = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
+        for (int i = 1; i <= 7; i++) {
             AtomicInteger weekday = new AtomicInteger(i);
             Integer weekdayCnt = listWeekdayStatsByShortLink.stream()
                     .filter(each -> Objects.equals(each.getWeekday(), weekday.get()))
